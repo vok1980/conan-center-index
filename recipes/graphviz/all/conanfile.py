d2aa38f8721a031149efd65b4daa4ea5e14f0633
Vyacheslav Koscheev <v.koscheev@2gis.ru>
2023-12-12 17:07:59 +0700

    Create graphviz recipe

---
+++ recipes/graphviz/all/conanfile.py
@@ -0,0 +1,58 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.apple import is_apple_os
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, replace_in_file, rm, rmdir
+from conan.tools.microsoft import is_msvc
+from conan.tools.scm import Version
+import os
+
+
+class GraphvizConan(ConanFile):
+    name = "graphviz"
+    description = "Graphviz is open source graph visualization software."
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://graphviz.org/"
+    license = "Eclipse Public License 1.0"
+    topics = ("graphics")
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        tc = CMakeDeps(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+        if self.options.shared:
+            rm(self, "*[!.dll]", os.path.join(self.package_folder, "bin"))
+        else:
+            rmdir(self, os.path.join(self.package_folder, "bin"))
+
